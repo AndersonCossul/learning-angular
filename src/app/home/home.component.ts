69247324ef5c56ee886dcd35e7f6e1ceafd3ef55
Anderson Cossul <anderson_cossul@hotmail.com>
2019-01-30 23:05:56 -0200

    dynamic css

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  h1GrayStyle: boolean = false
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  firstClick() {
+    this.h1GrayStyle = !this.h1GrayStyle;
+  }
 }
